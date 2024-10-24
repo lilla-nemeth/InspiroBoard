5cc675b53b7c5a68300707b5047972aa6807d119
Lilla Németh <naemly@protonmail.com>
2024-10-24 14:19:52 +0300

    creating context menu with 2 list items, using css class to make it visible and remove that when user clicks outside of list items; removing test console.log

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,40 @@ const todoList = <HTMLElement>document.getElementById('todo-list');
 // const cooking = <HTMLElement>document.getElementById('cooking');
 // const running = <HTMLElement>document.getElementById('running');
 
-const addCustomContextMenu = (e: MouseEvent) => {
+const contextMenu = document.createElement('div');
+contextMenu.className = 'context-menu';
+
+const contextList = document.createElement('ul');
+
+const contextDelete = document.createElement('li');
+contextDelete.className = 'context-menu-actions';
+contextDelete.id = 'context-menu-delete';
+contextDelete.textContent = 'Delete';
+contextList.appendChild(contextDelete);
+
+const contextEdit = document.createElement('li');
+contextEdit.className = 'context-menu-actions';
+contextEdit.id = 'context-menu-edit';
+contextEdit.textContent = 'Edit';
+contextList.appendChild(contextEdit);
+
+contextMenu.appendChild(contextList);
+document.body.appendChild(contextMenu);
+
+const addContextMenu = (e: MouseEvent) => {
 	e.preventDefault();
-	console.log('right-click');
+
+	contextMenu.classList.add('visible');
+	contextMenu.style.left = `${e.pageX}px`;
+	contextMenu.style.top = `${e.pageY}px`;
+};
+
+const hideContextMenu = () => {
+	contextMenu.classList.remove('visible');
 };
 
 if (todoList) {
-	todoList.addEventListener('contextmenu', addCustomContextMenu);
+	todoList.addEventListener('contextmenu', addContextMenu);
 }
+
+document.addEventListener('click', hideContextMenu);
